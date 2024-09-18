76255dd6c742e8d40038858cd6c348c82635b08d
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-17 18:34:50 -0700

    feat: Pipeline interfaces

---
+++ backend/src/models/Job.ts
@@ -0,0 +1,8 @@
+  export interface Job {
+    id: number;
+    stage_id: number;
+    name: string;
+    status: string;
+    created_at: string;
+  }
+  
---
+++ backend/src/models/Stage.ts
@@ -0,0 +1,8 @@
+export interface Stage {
+    id: number;
+    pipeline_id: number;
+    status: string;
+    name: string;
+    position: number;
+    created_at: string;
+}
