33e1f8c360e2775e6a23cee09e3c9dd1f653d92a
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-18 18:59:49 -0700

    feat: Init supabase create Client

---
+++ frontend/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! || 'https://ezojlihknxyowdmalmwx.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6b2psaWhrbnh5b3dkbWFsbXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYwODY3OTMsImV4cCI6MjA0MTY2Mjc5M30.0Q4UT7XYlKSiC-p4QuDoceMYzPVl8Yd6Z_srYSPSvH8';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
