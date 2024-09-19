f78513cd8aa67346bbb40130725736b0a732b583
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-18 19:00:07 -0700

    feat: route configuration

---
+++ frontend/lib/routes.ts
@@ -0,0 +1,3 @@
+export const publicRoutes = ['/sign-in', '/sign-up', '/products', '/'];
+export const authRoutes = ['/sign-in', '/sign-up'];
+export const protectedRoutes = ['/dashboard', '/profile'];
