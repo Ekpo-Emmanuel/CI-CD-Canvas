62007ebbada7331710aa7544c0da0776a75d8eb4
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-09-18 19:02:58 -0700

    feat(Auth): added middleware solution

---
+++ frontend/middleware.ts
@@ -0,0 +1,26 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (user && (req.nextUrl.pathname === '/sign-in' || req.nextUrl.pathname === '/sign-up')) {
+    return NextResponse.redirect(new URL('/dashboard', req.url))
+  }
+
+  if (!user && req.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/sign-in', '/sign-up', '/dashboard/:path*'],
+}
